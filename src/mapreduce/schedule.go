125168c8a1c4d794c0f9f956505abaf1613370d2
R3gardless <pidaoh@g.skku.edu>
2024-06-01 19:43:46 +0900

    chore: 📝 Add comment to schedule.go

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -19,10 +19,14 @@ func (mr *Master) schedule(phase jobPhase) {
 	// them have been completed successfully should the function return.
 	// Remember that workers may fail, and that any given worker may finish
 	// multiple tasks.
+
+	// Create a buffered channel to hold task indicies
 	tasks := make(chan int, ntasks)
 	for i := 0; i < ntasks; i++ {
-		tasks <- i
+		tasks <- i // Fill the channel with task indices
 	}
+
+	// Continue scheduling tasks until every tasks are completed
 	for len(tasks) > 0 {
 		processTask(mr.jobName, phase, mr.files, mr.registerChannel, tasks, len(tasks), nios)
 	}
@@ -30,27 +34,32 @@ func (mr *Master) schedule(phase jobPhase) {
 	debug("Schedule: %v phase done\n", phase)
 }
 
-func processTask(jobName string, phase jobPhase, mapFiles []string, registerChan chan string, tasks chan int, ntasks int, n_other int) {
-	nworkers := make(chan int)
-	for len(tasks) > 0 {
-		t := <-tasks
+func processTask(jobName string, phase jobPhase, files []string, registerChannel chan string, tasks chan int, ntasks int, nios int) {
+	nworkers := make(chan int) // Channel to track worker status
+	for len(tasks) > 0 {       // Continue until all tasks are processed
+		t := <-tasks // Get the next task from the channel
 		var task DoTaskArgs
 		switch phase {
 		case mapPhase:
-			task = DoTaskArgs{jobName, mapFiles[t], mapPhase, t, n_other}
+			task = DoTaskArgs{jobName, files[t], mapPhase, t, nios}
 		case reducePhase:
-			task = DoTaskArgs{jobName, "", reducePhase, t, n_other}
+			task = DoTaskArgs{jobName, "", reducePhase, t, nios}
 		}
+
+		// Launch a goroutine to handle the task
 		go func(task DoTaskArgs) {
-			address := <-registerChan
+			address := <-registerChannel
 			// DoTask is called by the master when a new task is being scheduled on this worker.
 			// func (wk *Worker) DoTask(arg *DoTaskArgs, _ *struct{})
+
+			// Schedule the task on the worker
 			if call(address, "Worker.DoTask", task, nil) {
 				nworkers <- 1
-				registerChan <- address
+				registerChannel <- address
 			} else {
-				nworkers <- 0
-				tasks <- task.TaskNumber
+				// log.Printf("Task %d failed on worker %s\n", task.TaskNumber, address) // Log the failure
+				nworkers <- 0            // Task failed
+				tasks <- task.TaskNumber // Requeue the task
 			}
 		}(task)
 	}
